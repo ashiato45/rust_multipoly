0c29afb71d9218f3c0a3dad0a612498b0eab09b9
ashiato45 <katiiijp@gmail.com>
2017-10-14 17:33:32 +0900

    changed to rational

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ extern crate num;
 
 use num::FromPrimitive;
 // use num::bigint::BigInt;
-use num::rational::{Ratio, BigRational};
+use num::rational::{Ratio, BigRational, Rational};
 
 fn main() {
     println!("Hello, world!");
     // println!("{}", Ratio::from_integer(1)/Ratio::from_integer(2));
-    println!("{}", make_bigrat(1, 2));
+    println!("{}", make_rat(1, 2));
 }
 
-trait Field{
+trait Field: Sized{
     fn zero() -> Self;
     fn one() -> Self;
     fn minus(&self) -> Self;
-    // fn recipro(&self) -> Option<Self>;
+    fn recipro(&self) -> Option<Self>;
     // fn subtr(&self), x: &Self, y: &Self) -> Self {
     //     self.add(x, y.minus())
     // }
@@ -23,22 +23,26 @@ trait Field{
     // fn div(&self);
 }
 
-impl Field for BigRational{
-    fn zero() -> BigRational{
-        make_bigrat(0, 1)
+impl Field for Rational{
+    fn zero() -> Rational{
+        make_rat(0, 1)
     }
-    fn one() -> BigRational{
-        make_bigrat(1, 1)
+    fn one() -> Rational{
+        make_rat(1, 1)
     }
-    fn minus(&self) -> BigRational{
+    fn minus(&self) -> Rational{
         -self
     }
-    // fn recipro(&self, x: &BigRational) -> Option<BigRational>{
-    //     if(x == )
-    // }
+    fn recipro(&self) -> Option<Rational>{
+        if(*self == Self::zero()){
+            None
+        }else{
+            Some(Self::one()/(*self))
+        }
+    }
 
 }
 
-fn make_bigrat(num: u64, den: u64) -> BigRational{
+fn make_rat(num: u64, den: u64) -> Rational{
     Ratio::from_integer(FromPrimitive::from_u64(num).unwrap())/Ratio::from_integer(FromPrimitive::from_u64(den).unwrap())
 }
